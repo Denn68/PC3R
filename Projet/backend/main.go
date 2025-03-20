de2db3b2fa4a456d99f8841d6253f9d36826cbf1
DENN <28608482@ppti-14-407-15.ufr-info-p6.jussieu.fr>
2025-03-20 12:30:08 +0100

    main pour mouf

--- Projet/backend/main.go
+++ Projet/backend/main.go
@@ -9,12 +9,33 @@ import (
 	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
+
+
+// Structure pour stocker une catégorie
+type Category struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+}
+
+type FilmPreviews struct {
+    Id          int  `json:"id"` 
+    Title       string  `json:"title"`
+    ReleaseDate string  `json:"release_date"`
+    Poster      []byte  `json:"poster"`
+    AverageRate float64 `json:"average_rate"`
+    NbRate      int     `json:"nb_rate"`
+}
+
+"""
 // Structure pour stocker un film
 type Film struct {
 	ID       int
 	Title    string
 	Overview string
 }
+"""
 
 // Fonction pour établir la connexion à la base de données
 func connectToDB() (*sql.DB, error) {
@@ -34,73 +55,231 @@ func connectToDB() (*sql.DB, error) {
 	return db, nil
 }
 
-// Fonction pour récupérer les films depuis la base de données
-func getFilmsFromDB(db *sql.DB) ([]Film, error) {
-	// Requête SQL pour récupérer les films
-	rows, err := db.Query("SELECT film_id, title, overview FROM films")
+func getCategoriesFromDB(db *sql.DB) ([]Category, error) {
+	// Requête SQL pour récupérer les catégories distinctes
+	rows, err := db.Query("SELECT * FROM categories")
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var films []Film
+	var categories []Category
 
 	// Parcours des résultats
 	for rows.Next() {
-		var film Film
-		err := rows.Scan(&film.ID, &film.Title, &film.Overview)
+		var category Category
+		if err := rows.Scan(&category.Id, &category.Name); err != nil {
+			return nil, err
+		}
+		categories = append(categories, category)
+	}
+
+	return categories, nil
+}
+
+// Fonction qui récupere toutes les catégories disponibles
+func getCategories(w http.ResponseWriter, r *http.Request) {
+	
+	// Récupérer les catégories depuis la db
+	categories, err := getCategoriesFromDB(db)
+	if err != nil {
+		http.Error(w, "Erreur lors de la récupération des catégories", http.StatusInternalServerError)
+		log.Println("Erreur lors de la récupération des catégories:", err)
+		return
+	}
+
+	// Définir l'en-tête de réponse pour JSON
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// Encoder les catégories en JSON et les envoyer dans la réponse
+	if err := json.NewEncoder(w).Encode(categories); err != nil {
+		http.Error(w, "Erreur d'encodage JSON", http.StatusInternalServerError)
+		log.Println("Erreur d'encodage JSON:", err)
+	}
+}
+
+func fetchPoster(posterPath string) ([]byte, error) {
+	baseURL := "https://image.tmdb.org/t/p/w500"
+	fullURL := baseURL + posterPath
+
+	resp, err := http.Get(fullURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("erreur HTTP: %d", resp.StatusCode)
+	}
+
+	imageData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return imageData, nil
+}
+
+func getFilmsByCategoriesFromDB(db *sql.DB, categoryId int) ([]FilmPreviews, error) {
+	
+	// Requête SQL pour récupérer les id de films par catégories
+	rows, err := db.Query("SELECT film_id FROM film_categories WHERE category_id = $1", categoryId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var filmsPreviews []FilmPreviews
+	for rows.Next() {
+		var filmId int
+		if err := rows.Scan(&filmId); err != nil {
+			return nil, err
+		}
+
+		var posterPath string
+		// Récupérer les infos du film
+		var film FilmPreviews
+		err := db.QueryRow("SELECT film_id, title, release_date, poster_path, average_rate, nb_rate FROM films WHERE film_id = $1", filmId).
+			Scan(&film.Id, &film.Title, &film.ReleaseDate, &posterPath, &film.AverageRate, &film.NbRate)
 		if err != nil {
 			return nil, err
 		}
-		films = append(films, film)
-	}
 
-	return films, nil
+		// Télécharger le poster
+		film.Poster, err = fetchPoster(posterPath)
+		if err != nil {
+			return nil, err
+		}
+
+		filmsPreviews = append(filmsPreviews, film)
+	}
+	return filmsPreviews, nil
 }
 
-// Fonction qui gère la route "/" et récupère les films de la base de données
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	// Connexion à la base de données
-	db, err := connectToDB()
-	if err != nil {
-		http.Error(w, "Erreur de connexion à la base de données", http.StatusInternalServerError)
-		log.Println("Erreur de connexion à la base de données:", err)
+func getFilmsByCategories(w http.ResponseWriter, r *http.Request) {
+
+	categoryIdStr := r.URL.Query().Get("category_id")
+	if categoryIdStr == "" {
+		http.Error(w, "Paramètre 'category_id' manquant", http.StatusBadRequest)
 		return
 	}
-	defer db.Close()
 
-	// Récupérer les films depuis la base de données
-	films, err := getFilmsFromDB(db)
+	// Convertir `category_id` en `int`
+	categoryId, err := strconv.Atoi(categoryIdStr)
+	if err != nil {
+		http.Error(w, "Paramètre 'category_id' invalide", http.StatusBadRequest)
+		return
+	}
+
+	// Récupérer les films depuis la db
+	filmPreviews, err := getFilmsByCategoriesFromDB(db, categoryId)
 	if err != nil {
 		http.Error(w, "Erreur lors de la récupération des films", http.StatusInternalServerError)
 		log.Println("Erreur lors de la récupération des films:", err)
 		return
 	}
+
+	// Définir l'en-tête de réponse pour JSON
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// Encoder les films en JSON et les envoyer dans la réponse
+	if err := json.NewEncoder(w).Encode(filmPreviews); err != nil {
+		http.Error(w, "Erreur d'encodage JSON", http.StatusInternalServerError)
+		log.Println("Erreur d'encodage JSON:", err)
+	}
 }
 
-// Fonction qui gère la route "/about" et répond avec un message simple
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Ceci est la page 'À propos'.")
+func getFilmByIdFromDB(db *sql.DB, categoryId int) ([]FilmPreviews, error) {
+	
+	// Requête SQL pour récupérer les id de films par catégories
+	rows, err := db.Query("SELECT film_id FROM film_categories WHERE category_id = $1", categoryId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var filmsPreviews []FilmPreviews
+	for rows.Next() {
+		var filmId int
+		if err := rows.Scan(&filmId); err != nil {
+			return nil, err
+		}
+
+		var posterPath string
+		// Récupérer les infos du film
+		var film FilmPreviews
+		err := db.QueryRow("SELECT film_id, title, release_date, poster_path, average_rate, nb_rate FROM films WHERE film_id = $1", filmId).
+			Scan(&film.Id, &film.Title, &film.ReleaseDate, &posterPath, &film.AverageRate, &film.NbRate)
+		if err != nil {
+			return nil, err
+		}
+
+		// Télécharger le poster
+		film.Poster, err = fetchPoster(posterPath)
+		if err != nil {
+			return nil, err
+		}
+
+		filmsPreviews = append(filmsPreviews, film)
+	}
+	return filmsPreviews, nil
 }
 
-// Fonction qui gère la route "/contact" et répond avec un message simple
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Ceci est la page 'Contact'.")
+func getFilmById(w http.ResponseWriter, r *http.Request) {
+
+	categoryIdStr := r.URL.Query().Get("category_id")
+	if categoryIdStr == "" {
+		http.Error(w, "Paramètre 'category_id' manquant", http.StatusBadRequest)
+		return
+	}
+
+	// Convertir `category_id` en `int`
+	categoryId, err := strconv.Atoi(categoryIdStr)
+	if err != nil {
+		http.Error(w, "Paramètre 'category_id' invalide", http.StatusBadRequest)
+		return
+	}
+
+	// Récupérer les films depuis la db
+	filmPreviews, err := getFilmsByCategoriesFromDB(db, categoryId)
+	if err != nil {
+		http.Error(w, "Erreur lors de la récupération des films", http.StatusInternalServerError)
+		log.Println("Erreur lors de la récupération des films:", err)
+		return
+	}
+
+	// Définir l'en-tête de réponse pour JSON
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// Encoder les films en JSON et les envoyer dans la réponse
+	if err := json.NewEncoder(w).Encode(filmPreviews); err != nil {
+		http.Error(w, "Erreur d'encodage JSON", http.StatusInternalServerError)
+		log.Println("Erreur d'encodage JSON:", err)
+	}
 }
 
 func main() {
+	// Connexion à la base de données
+	db, err := connectToDB()
+	if err != nil {
+		log.Fatal("Erreur de connexion à la base de données :", err)
+	}
+	defer db.Close()
+
 	// Définir les routes
-	http.HandleFunc("/", homeHandler)       // Route pour l'accueil
-	http.HandleFunc("/about", aboutHandler) // Route pour 'À propos'
-	http.HandleFunc("/contact", contactHandler) // Route pour 'Contact'
+	http.HandleFunc("/categories", getCategories)       // Route pour récuperer les catégories
+	http.HandleFunc("/categories/getFilms", getFilmsByCategories) // Route pour récuperer les films par catégorie
+	http.HandleFund("/films/getById", getFilmById) // Route pour récuperer un film par son id
 
 	// Définir le port d'écoute
 	port := ":8080"
 	fmt.Printf("Serveur démarré sur http://localhost%s\n", port)
 
 	// Démarrer le serveur HTTP
-	err := http.ListenAndServe(port, nil)
-	if err != nil {
+	if err = http.ListenAndServe(port, nil); err != nil {
 		log.Fatal("Erreur lors du démarrage du serveur : ", err)
 	}
 }
