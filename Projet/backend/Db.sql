7d0d1f332c37316fbac8a95cd86b72afd3d88811
DENN <28608482@ppti-14-407-15.ufr-info-p6.jussieu.fr>
2025-03-20 11:47:30 +0100

    Db pour mouf

--- Projet/backend/Db.sql
+++ Projet/backend/Db.sql
@@ -1,21 +1,34 @@
-CREATE TABLE films (
+CREATE TABLE IF NOT EXISTS films (
     film_id SERIAL PRIMARY KEY,  
     title VARCHAR(255) NOT NULL,
     overview TEXT,
     release_date DATE,
-    category TEXT,
-    poster_path VARCHAR(255)
+    poster_path VARCHAR(255),
+    average_rate DECIMAL(3,1) CHECK(average_rate >= 1.0 AND average_rate <= 10.0),
+    nb_rate INT DEFAULT 0
 );
 
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,  
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS categories (
+    category_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS film_categories (
+    film_id INT,
+    category_id INT,
+    PRIMARY KEY (film_id, category_id),
+    FOREIGN KEY (film_id) REFERENCES films(film_id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE CASCADE
+);
 
-CREATE TABLE ratings (
-    user_id INT,
+CREATE TABLE IF NOT EXISTS users (
+    user_id VARCHAR(255) PRIMARY KEY,  
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password_hash BYTEA NOT NULL  
+);
+
+CREATE TABLE IF NOT EXISTS ratings (
+    user_id VARCHAR(255),
     film_id INT,
     rating INT CHECK(rating >= 1 AND rating <= 10),
     rated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
